9568d83cd6dff2903e3b5d842286580d69e611e2
ArohanD <36905484+ArohanD@users.noreply.github.com>
2019-09-24 11:03:51 -0700

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.static('dist'));
+
+
+//Add new routes above
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, '../dist/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
+
+app.listen(port, () => console.log(`listening from port: ${port}`));
